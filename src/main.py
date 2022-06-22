1295bb0455f7ff0ade8c4acd493521c58e0410d2
arques89 <arquesjavier@gmail.com>
2022-06-22 14:08:53 +0000

    Ejercicio hecho

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Task
 #from models import Person
 
 app = Flask(__name__)
@@ -26,9 +26,59 @@ def handle_invalid_usage(error):
     return jsonify(error.to_dict()), error.status_code
 
 # generate sitemap with all your endpoints
-@app.route('/')
-def sitemap():
+
+@app.route('/') 
+def sitemap(): 
     return generate_sitemap(app)
+    
+@app.route('/task' , methods=['GET']) 
+def get_tasks():
+    tasks = Task.query.all()
+    all_tasks = list(map(lambda task: task.serialize(),tasks))
+    return jsonify(all_tasks)
+
+@app.route('/task' , methods=['POST']) 
+def create_tasks():
+    body = request.get_json()
+    task = Task(text=body["text"], done=False)
+    db.session.add(task)
+    db.session.commit()
+
+    res = jsonify(task.serialize())
+    res.status_code = 201
+    return res
+
+@app.route('/task/<int:task_id>' , methods=['PUT' , 'GET' , 'DELETE']) 
+def handle_task(task_id):
+
+    if request.method == 'GET':
+        task = Task.query.get(task_id)
+        if task is None:
+            raise APIException("Tarea no encontrada", 404)
+
+        return jsonify(task.serialize())
+
+    elif request.method == 'PUT':
+        task = Task.query.get(task_id)
+        if task is None:
+            raise APIException("Tarea no encontrada", 404)
+        body = request.get_json()
+        if not ("done" in body):
+            raise APIException("parametro no encontrado", 404)
+        task.done = body["done"]
+        db.session.commit()
+
+        return jsonify(task.serialize())
+
+    elif request.method == 'DELETE':
+        task = Task.query.get(task_id)
+        if task is None:
+            raise APIException("Tarea no encontrada", 404)
+        db.session.delete(task)   
+        db.session.commit()   
+        
+        return jsonify(task.serialize())  
+    
 
 @app.route('/user', methods=['GET'])
 def handle_hello():
